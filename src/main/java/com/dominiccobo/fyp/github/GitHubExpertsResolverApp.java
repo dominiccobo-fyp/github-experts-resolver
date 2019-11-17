2a47eef0985c0b0b492e6870bcb60745fb2fd3cf
Dominic Cobo <dominiccobo@gmail.com>
2019-11-17 22:49:52 +0000

    update imports

--- src/main/java/com/dominiccobo/fyp/github/GitHubExpertsResolverApp.java
+++ src/main/java/com/dominiccobo/fyp/github/GitHubExpertsResolverApp.java
@@ -1,5 +1,8 @@
 package com.dominiccobo.fyp.github;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class GitHubExpertsResolverApp {
     public static void main(String[] args) {
