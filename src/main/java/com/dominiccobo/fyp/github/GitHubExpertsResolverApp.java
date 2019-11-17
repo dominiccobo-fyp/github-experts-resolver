fd94e40f66ce636d2bba0b2a2a3c9020dc2535c1
Dominic Cobo <dominiccobo@gmail.com>
2019-11-17 22:39:38 +0000

    Initial commit

---
+++ src/main/java/com/dominiccobo/fyp/github/GitHubExpertsResolverApp.java
@@ -0,0 +1,8 @@
+package com.dominiccobo.fyp.github;
+
+@SpringBootApplication
+public class GitHubExpertsResolverApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubExpertsResolverApp.class, args);
+    }
+}
